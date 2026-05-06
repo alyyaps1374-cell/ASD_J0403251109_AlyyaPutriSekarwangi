80fbb8b739e168d68bc459847b727570688ba6f9
alyyaps1374-cell <alyyaps1374@gmail.com>
2026-05-06 16:59:40 +0700

    Implement Bellman-Ford algorithm in Python
    This code implements the Bellman-Ford algorithm to find the shortest paths from a starting node to all other nodes in a graph. It initializes distances to infinity, except for the starting node, and performs relaxation for all edges.

--- Pertemuan 12/praktikum12.materi2.py
+++ Pertemuan 12/praktikum12.materi2.py
@@ -5,7 +5,7 @@
 #=================================================
 
 # ==========================================================
-# Materi 1: Implementasi Bellman Ford dalam Phyton.
+# Materi 2: Implementasi Bellman Ford dalam Phyton.
 # ==========================================================
 
 def bellman_ford(graph, start):
@@ -23,3 +23,14 @@ def bellman_ford(graph, start):
                 if distances[node] + weight < distances[neighbor]:
                     distances[neighbor] = distances[node] + weight
     return distances
+
+#PENJELASAN: 
+# Kode ini mengimplementasikan algoritma Bellman-Ford untuk mencari jalur terpendek dari 
+# node awal ke semua node lain dalam graph. Alurnya dimulai dengan menginisialisasi jarak 
+# ke semua node dengan nilai tak terhingga (inf), kecuali node awal yang disetel menjadi 0.
+# Setelah itu, kode melakukan proses relaksasi dengan mengevaluasi seluruh sisi 
+# di dalam graf sebanyak total node dikurangi satu (len(graph) - 1). Pada setiap evaluasi,
+# jika jarak menuju suatu node ditambah bobot jalurnya ternyata lebih kecil/singkat dari 
+# jarak node tetangganya yang tersimpan saat ini, maka nilai jarak tetangga tersebut akan
+# diperbarui ke nilai yang baru. Pada akhirnya, fungsi akan mengembalikan dictionary yang
+# berisi data jarak terpendek dari node awal ke masing-masing node di dalam graf tersebut.
